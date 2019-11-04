8e82d8dd084753b4daa81c6d00983751c7ef7b41
Madeline Corman <madelinecorman@gmail.com>
2019-11-04 16:47:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -51,7 +51,7 @@ def using_flatten(instruments)
 end
 
 def using_delete(instructors)
-  no_offense_steven = instructors.delete('Steven')
+  no_offense_steven = instructors.delete("Steven")
 end
 
 
