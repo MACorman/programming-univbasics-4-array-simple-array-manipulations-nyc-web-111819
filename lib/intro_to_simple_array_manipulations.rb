104f6a85fb7f5b2716cf0443391f68eef1b19bae
Madeline Corman <madelinecorman@gmail.com>
2019-11-04 16:55:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -55,8 +55,8 @@ def using_delete(instructors, remove_steven)
   no_offense_steven = instructors.delete(remove_steven)
 end
 
-def using_delete_at(famous_robots, index)
-  index = 2
-  deleted_robot = famous_robots.delete(index)
+def using_delete_at(famous_robots, robocop)
+  robocop = 2
+  deleted_robot = famous_robots.delete(robocop)
 end
 
